d2e64e66778bf405ac9d1248852b63f1ec2c80c9
이동언 <80028638+47813@users.noreply.github.com>
2021-09-16 08:11:35 +0900

    Create 05_practice_2.ino
    Code for LED to blink for each second for 5 times and turn off the LED

---
+++ 05_practice_2.ino
@@ -0,0 +1,31 @@
+#define PIN7 7
+void setup() {
+  pinMode(PIN7, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(PIN7,LOW);
+  delay(1000);
+  digitalWrite(PIN7, HIGH);
+  delay(1000);
+  digitalWrite(PIN7,LOW);
+  delay(1000);
+  digitalWrite(PIN7, HIGH);
+  delay(1000);
+  digitalWrite(PIN7,LOW);
+  delay(1000);
+  digitalWrite(PIN7, HIGH);
+  delay(1000);
+  digitalWrite(PIN7,LOW);
+  delay(1000);
+  digitalWrite(PIN7, HIGH);
+  delay(1000);
+  digitalWrite(PIN7,LOW);
+  delay(1000);
+  digitalWrite(PIN7, HIGH);
+  delay(1000);
+  while(1){
+    digitalWrite(PIN7, HIGH);
+  }
+
+}
